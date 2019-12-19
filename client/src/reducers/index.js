fb64bd8c3bc836083a9547f724e0307076effea9
kiingdavies <daviesadedeji007@gmail.com>
2019-12-19 11:46:55 +0100

    StreamList Done

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'; //we renamed the named import reducer to formReducer
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
    auth: authReducer,
-   form: formReducer
+   form: formReducer,
+   streams: streamReducer
 });
